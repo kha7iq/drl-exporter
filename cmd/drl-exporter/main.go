6354e3e1aa4c643ecf259aa42fdcbbb20d744938
khaliq <a.khaliq@outlook.my>
2024-08-31 16:17:55 +0800

    refactor:  metrics collection with configurable interval initialization

--- cmd/drl-exporter/main.go
+++ cmd/drl-exporter/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	prom "github.com/kha7iq/drl-exporter/internal/prom"
@@ -23,6 +24,7 @@ func main() {
 	if err := env.Parse(); err != nil {
 		log.Println(err)
 	}
+
 	mux := http.NewServeMux()
 	s := &http.Server{
 		Addr:         ":" + *vars.BindAddress,
@@ -32,17 +34,26 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
-	prom.RegisterCollectors()
-	prom.CollectMetrics()
+	rqs := *vars.RequestInterval
+	interval, err := strconv.Atoi(rqs)
+	if err != nil {
+		log.Printf("Invalid interval value '%s', defaulting to 10 seconds: %v\n", rqs, err)
+		interval = 10
+	}
+	// Convert interval to a time.Duration
+	duration := time.Duration(interval) * time.Second
+
+	// Initialize and start metrics collection with a configurable interval
+	prom.InitMetrics()
+	prom.StartMetricsCollection(duration)
 
 	mux.Handle("/metrics", promhttp.Handler())
 	appInfo(version, buildDate, commitSha)
 
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
 func appInfo(version, buildDate, commit string) {
